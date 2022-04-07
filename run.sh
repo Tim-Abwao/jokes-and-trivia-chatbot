d0491165cf1b756f1ada4ef0716775c90c5b0088
Tim-Abwao <abwaomusungu@gmail.com>
2022-04-07 22:31:00 +0300

    Update run.sh
    Automatically launch browser to demo web-page.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Activate the Python virtual environment
+# Activate the virtual environment, if present
 if [ -d venv ]
     then
         source venv/bin/activate
@@ -13,6 +13,14 @@ if [ ! -d models ]
     then rasa train
 fi
 
-# Start the action server & Rasa X
+# Start the action server in the background
 rasa run actions &
-rasa x
+
+# Open the demo web-page in a browser
+python -m webbrowser "../demo-webpage.html"
+
+# Start the rasa server
+rasa run --cors="*"
+
+# Return to chatbot home "jokes-and-trivia-chatbot"
+cd ..
